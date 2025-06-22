c56d3d168c7896941854c7ead906ad1a676c9d8a
NinaDEV <168221598+NinaDIV@users.noreply.github.com>
2025-06-21 20:31:17 -0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Milward on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
